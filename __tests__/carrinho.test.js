b10aaa9399593d0ef7b024238d3ccf6085ebe053
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-12-20 17:14:41 -0300

    cart tests

--- __tests__/carrinho.test.js
+++ __tests__/carrinho.test.js
@@ -21,6 +21,40 @@ describe('Cart tests', () => {
 
         expect(cart.itens).toContain(item);
         expect(cart.itens).toContain(item2);
+    })
 
+    it('Should have the total property in the inicialization', () => {
+        const cart = new Carrinho();
+        expect(cart).toHaveProperty('total');
+    })
+
+    it('Should throw error if cart is empty', () => {
+        function getCartError() {
+            const cart = new Carrinho();
+            cart.finalizaCompra();
+        }
+        expect(getCartError).toThrowError('Carrinho de compras vazio');
+    });
+
+    it('Should add shipping', () => {
+        const cart = new Carrinho();
+        cart.adicionaFrete(10);
+        expect(cart.frete).toBe(10);
+    })
+
+    it('Should do the checkout', () => {
+        const item = new Item('Banana', 2, 5);
+        const item2 = new Item('Mel', 1, 5);
+
+        const cart = new Carrinho();
+        cart.adiciona(item);
+        cart.adiciona(item2);
+        cart.adicionaFrete(10);
+
+        expect(cart.finalizaCompra()).toStrictEqual({
+            subtotal: 15,
+            frete: 10,
+            total: 25
+        })
     })
 })
